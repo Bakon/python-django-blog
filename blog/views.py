b4be74cc5bbfd5ce632e466b141fad0480751175
Julio Schilders <julio.schilders@gmail.com>
2020-03-01 02:52:12 +0100

    Connected blog posts to users and added to database, styling of the blog post cards

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,11 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Julio',
-        'title': 'Blog Post 1',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam similique sunt, nulla repudiandae sequi rem minus facilis, culpa molestiae beatae consectetur illum in harum voluptatem fuga reprehenderit. Exercitationem, a. Consequuntur.',
-        'date_posted': '29-02-2020'
-    },
-    {
-        'author': 'Julio',
-        'title': 'Blog Post 2',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam similique sunt, nulla repudiandae sequi rem minus facilis, culpa molestiae beatae consectetur illum in harum voluptatem fuga reprehenderit. Exercitationem, a. Consequuntur.',
-        'date_posted': '29-02-2020'
-    },
-    {
-        'author': 'Julio',
-        'title': 'Blog Post 3',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam similique sunt, nulla repudiandae sequi rem minus facilis, culpa molestiae beatae consectetur illum in harum voluptatem fuga reprehenderit. Exercitationem, a. Consequuntur.',
-        'date_posted': '29-02-2020'
-    }
-]
+from .models import Post
 
 # render looks inside the templates directory
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
         'title': 'Blog - Home',
         'meta': {
             'title': 'Title home page',
